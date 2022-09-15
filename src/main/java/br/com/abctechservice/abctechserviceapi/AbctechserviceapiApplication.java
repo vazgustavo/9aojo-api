d6400609aff8709f182472b3a1ec8ad1341a3dff
gustavo vaz <gustavov@dbserver.com.br>
2022-09-15 16:49:16 -0300

    first commit

---
+++ src/main/java/br/com/abctechservice/abctechserviceapi/AbctechserviceapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.abctechservice.abctechserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbctechserviceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbctechserviceapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/abctechservice/abctechserviceapi/AbctechserviceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abctechservice.abctechserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbctechserviceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
