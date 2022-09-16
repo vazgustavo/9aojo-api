4ff951e54f273b3e9a876535618dd7d5bc188c98
gustavo vaz <vazcomz@yahoo.com>
2022-09-16 20:53:26 -0300

    add new models

---
+++ src/main/java/br/com/abctechservice/abctechserviceapi/model/Order.java
@@ -0,0 +1,36 @@
+package br.com.abctechservice.abctechserviceapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    
+    @Column(name = "operador_id", nullable = false)
+    private Long operatorId;
+    
+    @ManyToMany
+    private List<Assistance> services;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "start_order_location_id", foreignKey = @ForeignKey(name = "FK_start_order_id"))
+    private OrderLocation startOrderLocation;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "end_order_location_id", foreignKey = @ForeignKey(name = "FK_end_order_id"))
+    private OrderLocation endOrderLocation;
+}
