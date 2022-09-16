4be828fabb9d3ede129400815c4609d0e429220b
gustavo vaz <vazcomz@yahoo.com>
2022-09-16 14:48:22 -0300

    include piplines

--- src/main/java/br/com/abctechservice/abctechserviceapi/controller/HealthCheckController.java
+++ src/main/java/br/com/abctechservice/abctechserviceapi/controller/HealthCheckController.java
@@ -1,5 +1,8 @@
 package br.com.abctechservice.abctechserviceapi.controller;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HealthCheckController {
 
-    @GetMapping()
-    public ResponseEntity<Object> status() {
-        return ResponseEntity.ok().build();
+    @GetMapping("/version")
+    public ResponseEntity<String> status() throws Exception {
+    	Properties properties = new Properties();
+    	InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
+    	properties.load(inputStream);
+        return ResponseEntity.ok(properties.getProperty("build.name") + " - " + properties.getProperty("build.version"));
     }
 }
